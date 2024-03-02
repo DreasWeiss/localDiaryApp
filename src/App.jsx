c090229e399c1977ff82b9ece39433a0c357d668
Dreas Weiss <dreasweiss@gmail.com>
2024-03-02 13:54:43 +0300

    update DiaryItem

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,35 @@ import Button from './components/Button/Button';
 import DiaryItem from './components/DiaryItem/DiaryItem';
 
 function App() {
+
+	const data = [
+		{
+			title: 'Prepairing for course update',
+			date: new Date(),
+			text: 'Горные походы открывают удивительные природные ландшафт' 
+		},
+		{
+			title: 'Hiking to the mountains',
+			date: new Date(),
+			text: 'Различают альпинизм и горный туризм. Если в альпинизме главная цель' 
+		},
+		{
+			title: 'First note',
+			date: new Date(),
+			text: 'Горные походы открывают удивительные природные ландшафт' 
+		}
+	];
+
 	return <>
 		<h1>Hello World</h1>
 		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste accusantium, corporis repellendus quasi sint beatae nesciunt earum cumque alias tenetur debitis nostrum blanditiis fuga dolorum excepturi nisi voluptate, praesentium mollitia!</p>
 		<Button />
-		<DiaryItem />
+		<DiaryItem 
+			title={data[1].title}  
+			text={data[1].text} 
+			date={data[1].date}
+		/>  
+		<DiaryItem title={data[1].title} text={data[1].text} date={data[1].date}/>  
 	</>;
 }
 
--- src/components/DiaryItem/DiaryItem.jsx
+++ src/components/DiaryItem/DiaryItem.jsx
@@ -1,15 +1,24 @@
 import './DiaryItem.css';
 
-export default function DiaryItem() {
-	const title = 'Prepairing for course update';
-	const date = new Date();
-	const text = 'Горные походы открывают удивительные природные ландшафт'; 
+export default function DiaryItem({title, text, date}) {
+	
+	// const preatyDate = (date) => {
+	// 	let day = date.getDate();
+	// 	let month = date.getMonth();
+	// 	let year = date.getFullYear();
+
+	// 	return `${day < 10 ? '0' + day : day}.${month+1 < 10 ? '0' + month : month}.${year}`;
+	// };
+    
+	const formatedDate = new Intl.DateTimeFormat('ru-RU').format(date);
+ 
 
 	return (
 		<div className="diary-item">
 			<h2 className="diary-item__header">{title}</h2>
 			<h2 className="diary-item__body">
-				<div className="diary-item__date">{date.toString() }</div>
+				{/* <div className="diary-item__date">{preatyDate(date)}</div> */}
+				<div className="diary-item__date">{formatedDate }</div>
 				<div className="diary-item__text">{text}</div>
 			</h2>
 		</div>
