a78a07bccd47a8d86e8514e9f4bb8b0c161cf364
Dreas Weiss <dreasweiss@gmail.com>
2024-03-02 11:21:52 +0300

    add component Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Button from './components/Button/Button';
 
 function App() {
-	return <div className="">project</div>;
+	return <>
+		<h1>Hello World</h1>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste accusantium, corporis repellendus quasi sint beatae nesciunt earum cumque alias tenetur debitis nostrum blanditiis fuga dolorum excepturi nisi voluptate, praesentium mollitia!</p>
+		<Button />
+	</>;
 }
 
 export default App;
