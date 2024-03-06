867e42da8bce188f1abea428a808492872b8a0aa
Dreas Weiss <dreasweiss@gmail.com>
2024-03-06 15:27:01 +0300

    add minimal validation 4 DiaryForm

--- src/App.jsx
+++ src/App.jsx
@@ -14,19 +14,19 @@ const INITIAL_DATA = [
 	// 	id: 1,
 	// 	title: 'Prepairing for course update',
 	// 	date: new Date(),
-	// 	text: 'Горные походы открывают удивительные природные ландшафт' 
+	// 	post: 'Горные походы открывают удивительные природные ландшафт' 
 	// },
 	// {
 	// 	id: 2,
 	// 	title: 'Hiking to the mountains',
 	// 	date: new Date(),
-	// 	text: 'Различают альпинизм и горный туризм. Если в альпинизме главная цель' 
+	// 	post: 'Различают альпинизм и горный туризм. Если в альпинизме главная цель' 
 	// },
 	// {
 	// 	id: 3,
 	// 	title: 'First note',
 	// 	date: new Date(),
-	// 	text: 'Горные походы открывают удивительные природные ландшафт' 
+	// 	post: 'Горные походы открывают удивительные природные ландшафт' 
 	// }
 ];
 
@@ -36,7 +36,7 @@ function App() {
 
 	const addItem = (item) => {
 		setItems(oldItems=>[...oldItems, {
-			text: item.text,
+			post: item.text,
 			date: new Date(item.date),
 			title: item.title,
 			tag: item.tag,
--- src/components/DiaryForm/DiaryForm.jsx
+++ src/components/DiaryForm/DiaryForm.jsx
@@ -1,24 +1,52 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import './DiaryForm.css';
 
 import Button from '../Button/Button';
 
 export default function DiaryForm({onSubmit}) {
 
+	const [formValidState, setFormValidState] = useState({
+		title: true,
+		post: true,
+		date: true
+	});
+
 	const addDiaryItem = (e) => {
 		e.preventDefault();
 		const formData = new FormData(e.target);
 		const formProps = Object.fromEntries(formData);
-		onSubmit(formProps); 
+		let isFormValid = true;
+		if (!formProps.title?.trim().length) {
+			setFormValidState(state=>({...state, title: false}));
+			isFormValid = false;
+		} else {
+			setFormValidState(state=>({...state, title: true}));
+		}
+		if (!formProps.post?.trim().length) {
+			setFormValidState(state=>({...state, post: false}));
+			isFormValid = false;
+		} else {
+			setFormValidState(state=>({...state, post: true}));
+		}
+		if (!formProps.date) {
+			setFormValidState(state=>({...state, date: false}));
+			isFormValid = false;
+		} else {
+			setFormValidState(state=>({...state, date: true}));
+		}
+		if(!isFormValid){
+			return;
+		}
+		onSubmit(formProps);
 	};
 
 	return (
 		<form className='diary-form' onSubmit={addDiaryItem }>
-			<input type="text" name='title'/>
-			<input type="date" name='date'/>
+			<input type="text" name='title' style={{border: formValidState.title ? undefined : '1px solid red'}}/>
+			<input type="date" name='date' style={{border: formValidState.date ? undefined : '1px solid red'}}/>
 			<input type="text" name='tag'/> 
-			<textarea name="text" id="" cols="30" rows="10"></textarea>
-			<Button text={'Save'} onClick={()=>console.log('Clicked')}/>
+			<textarea name="post" id="" cols="30" rows="10" style={{border: formValidState.post ? undefined : '1px solid red'}}></textarea>
+			<Button text={'Save'}/>
 		</form>
 	);
 }
