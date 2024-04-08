8553242c85d5c1cf47d964f25aec6e0f0ea87135
Dreas Weiss <dreasweiss@gmail.com>
2024-04-08 21:47:24 +0300

    DairyForm optimized with useReducer

--- src/components/DiaryForm/DiaryForm.jsx
+++ src/components/DiaryForm/DiaryForm.jsx
@@ -1,15 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer } from 'react';
 import styles from './DiaryForm.module.css';
 import cn from 'classnames';
 // import classNames from 'classnames';
 
 import Button from '../Button/Button';
+import { INITIAL_STATE, formReducer } from './DiaryForm.state';
 
-const INITIAL_STATE = {
-	title: true,
-	post: true,
-	date: true
-};
+// const INITIAL_STATE = {
+// 	title: true,
+// 	post: true,
+// 	date: true
+// };
 
 export default function DiaryForm({onSubmit}) {
 
@@ -18,47 +19,70 @@ export default function DiaryForm({onSubmit}) {
 	// 	post: true,
 	// 	date: true
 	// });
-	const [formValidState, setFormValidState] = useState(INITIAL_STATE);
+	// const [formValidState, setFormValidState] = useState(INITIAL_STATE);
+
+	const [formState, dispatchForm] = useReducer(formReducer, INITIAL_STATE);
+	const {isValid, isFormReadyToSubmit, values} = formState;
 
 	useEffect(()=> {
 		let timerID;
-		if (!formValidState.title || !formValidState.post || !formValidState.date) {
+		if (!isValid.title || !isValid.post || !isValid.date) {
 			timerID = setTimeout(()=>{
-				setFormValidState(INITIAL_STATE);
+				dispatchForm({type: 'RESET_VALIDITY'});
 			}, 2000);
 		}
 		return () => {
 			clearTimeout(timerID);
 		};
-	},[formValidState]);
+	},[isValid]);
+
+	useEffect(()=>{
+		if (isFormReadyToSubmit) {
+			onSubmit(values);
+		}
+	},[isFormReadyToSubmit]);
+
+	// useEffect(()=> {
+	// 	let timerID;
+	// 	if (!formValidState.title || !formValidState.post || !formValidState.date) {
+	// 		timerID = setTimeout(()=>{
+	// 			setFormValidState(INITIAL_STATE);
+	// 		}, 2000);
+	// 	}
+	// 	return () => {
+	// 		clearTimeout(timerID);
+	// 	};
+	// },[formValidState]);
 
 	const addDiaryItem = (e) => {
 		e.preventDefault();
 		const formData = new FormData(e.target);
 		const formProps = Object.fromEntries(formData);
-		let isFormValid = true;
-		if (!formProps.title?.trim().length) {
-			setFormValidState(state=>({...state, title: false}));
-			isFormValid = false;
-		} else {
-			setFormValidState(state=>({...state, title: true}));
-		}
-		if (!formProps.post?.trim().length) {
-			setFormValidState(state=>({...state, post: false}));
-			isFormValid = false;
-		} else {
-			setFormValidState(state=>({...state, post: true}));
-		}
-		if (!formProps.date) {
-			setFormValidState(state=>({...state, date: false}));
-			isFormValid = false;
-		} else {
-			setFormValidState(state=>({...state, date: true}));
-		}
-		if(!isFormValid){
-			return;
-		}
-		onSubmit(formProps);
+
+		// let isFormValid = true;
+		// if (!formProps.title?.trim().length) {
+		// 	setFormValidState(state=>({...state, title: false}));
+		// 	isFormValid = false;
+		// } else {
+		// 	setFormValidState(state=>({...state, title: true}));
+		// }
+		// if (!formProps.post?.trim().length) {
+		// 	setFormValidState(state=>({...state, post: false}));
+		// 	isFormValid = false;
+		// } else {
+		// 	setFormValidState(state=>({...state, post: true}));
+		// }
+		// if (!formProps.date) {
+		// 	setFormValidState(state=>({...state, date: false}));
+		// 	isFormValid = false;
+		// } else {
+		// 	setFormValidState(state=>({...state, date: true}));
+		// }
+		// if(!isFormValid){
+		// 	return;
+		// }
+		// =>
+		dispatchForm({type: 'SUBMIT', payload: formProps});
 	};
 
 	return (
@@ -68,7 +92,7 @@ export default function DiaryForm({onSubmit}) {
 					type="text" 
 					name='title' 
 					className={cn(styles['input-title'], {
-						[styles.invalid] : !formValidState.title
+						[styles.invalid] : !isValid.title
 					})}/>
 				{/* className={cn(styles.input, styles.invalid )}/> */}
 				{/* className={`${styles['input']} ${formValidState.title ? '' : styles['invalid']}`}/> */}
@@ -99,7 +123,7 @@ export default function DiaryForm({onSubmit}) {
 					name='date' 
 					id='date'
 					className={cn(styles.input, {
-						[styles.invalid] : !formValidState.date
+						[styles.invalid] : !isValid.date
 					})}/>
 			</div>
 			<div className={styles['form-row']}>
@@ -124,7 +148,7 @@ export default function DiaryForm({onSubmit}) {
 				cols="30" 
 				rows="10" 
 				className={cn(styles.input, {
-					[styles.invalid]: !formValidState.post
+					[styles.invalid]: !isValid.post
 				})}>
 			</textarea>
 			<Button text={'Save'}/>
